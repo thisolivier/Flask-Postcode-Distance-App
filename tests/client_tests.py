d65e35ba740ba9a31ce8610c5cfbd79172b04c80
Olivier Butler <olivier.butler@gmail.com>
2019-06-30 16:22:48 +0100

    Added more test suites, stubs

---
+++ tests/client_tests.py
@@ -0,0 +1,17 @@
+import os
+import tempfile
+import unittest
+
+from context import clients
+
+class ClientTest(unittest.TestCase):
+    # function to set up testing connection
+    def setUp(self):
+        pass
+
+    # function to teardown connection after testing
+    def tearDown(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/longlats_tests.py
@@ -0,0 +1,17 @@
+import os
+import tempfile
+import unittest
+
+from context import longlats
+
+class LonglatsTest(unittest.TestCase):
+    # function to set up testing connection
+    def setUp(self):
+        pass
+
+    # function to teardown connection after testing
+    def tearDown(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
